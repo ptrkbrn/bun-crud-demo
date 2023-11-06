9e4d6c4c3ba2628a54fc804b5ea32133c560289d
Patrick Breen <pressure78@gmail.com>
2023-11-05 19:43:05 -0500

    create index.html

--- index.ts
+++ index.ts
@@ -1,1 +1,9 @@
-console.log("Hello via Bun!");
+import { Elysia } from 'elysia';
+import { html } from '@elysiajs/html';
+import { BooksDatabase } from './db';
+
+new Elysia()
+    .use(html())
+    .decorate('db', new BooksDatabase())
+    .listen(3000);
+    
